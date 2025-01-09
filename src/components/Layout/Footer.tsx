9900a8ea9dcb11a46e4e590fb7a850a08f67e26a
Xyves <xodrixx@protonmail.com>
2025-01-09 21:46:22 +0100

    Create footer component

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,16 +1,23 @@
-import { Container, Stack, Link } from "@chakra-ui/react";
+import { Stack, Link } from "@chakra-ui/react";
 import "primeicons/primeicons.css";
 
 export default function Footer() {
   return (
-    <Container>
-      <Stack>
-        <Stack direction="row" justify="space-between" align="center">
-          Github:
-          <i className="pi pi-github"></i>
-          <Link href="https://github.com/Xyves">Xyves</Link>
-        </Stack>
+    <Stack
+      position="fixed"
+      bottom="0"
+      padding="2"
+      background={"yellow.50"}
+      width={"full"}
+      color="black"
+    >
+      <Stack direction="row" align="center" justifyContent={"center"}>
+        Github:
+        <i className="pi pi-github text-center"></i>
+        <Link href="https://github.com/Xyves " className="text-center">
+          Xyves
+        </Link>
       </Stack>
-    </Container>
+    </Stack>
   );
 }
