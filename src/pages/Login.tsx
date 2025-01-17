d1b88000cf457b27fc1d20865241a5057797692e
Xyves <xodrixx@protonmail.com>
2025-01-17 23:00:22 +0100

    feat: Implement basic login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,52 @@
-import { Field, Fieldset, Input } from "@chakra-ui/react";
-import React from "react";
+import {
+  Badge,
+  Box,
+  Container,
+  FieldLabel,
+  Fieldset,
+  Input,
+  Stack,
+  Switch,
+} from "@chakra-ui/react";
+import { Field } from "../components/ui/field";
+import {
+  PasswordInput,
+  PasswordStrengthMeter,
+} from "../components/ui/password-input";
+import { useState } from "react";
+
 export default function Login() {
+  const [value, setValue] = useState("");
   return (
     <div>
-      <form action="">
-        <Fieldset.Content>
-          <Field label="Name">
-            <Input name="name" />
-          </Field>
-        </Fieldset.Content>
-      </form>
+      <Container background={"#1C2321"}>
+        <Box>
+          <Stack gap="8" maxW="sm">
+            <Field orientation="horizontal" label="Name" required>
+              <Input placeholder="John Doe" flex="1" />
+            </Field>
+            <Stack maxW="300px">
+              <Field orientation="horizontal" label="Password" required>
+                <PasswordInput
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                />
+              </Field>
+            </Stack>
+            <Field
+              orientation="horizontal"
+              label="Email"
+              optionalText={
+                <Badge size="xs" variant="surface">
+                  Optional
+                </Badge>
+              }
+            >
+              <Input placeholder="me@example.com" flex="1" />
+            </Field>
+          </Stack>
+        </Box>
+      </Container>
     </div>
   );
 }
