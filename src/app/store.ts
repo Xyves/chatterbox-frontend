48fa6ad736773aa2472e110c6d0f85730a6883eb
Xyves <xodrixx@protonmail.com>
2025-01-30 22:11:56 +0100

    Create redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+// @ts-expect-error
+import authReducer from "../features/authSlice.js";
+import persistStore from "redux-persist/es/persistStore";
+import persistReducer from "redux-persist/es/persistReducer";
+const persistConfig = {
+  key: "root",
+};
 
-export default configureStore({
-  reducer: {},
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: {
+    auth: persistedReducer,
+  },
 });
+const persistor = persistStore(store);
+
+export { store, persistor };
