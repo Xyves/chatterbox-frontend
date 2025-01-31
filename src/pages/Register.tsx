10256c22049993797b9b122ccd760bf10accdbee
Xyves <xodrixx@protonmail.com>
2025-01-31 21:56:44 +0100

    feat: Create submitting form logic for registering

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,40 +1,63 @@
-import { Box, Flex, Heading, Input, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
+import { Box, Flex, Heading, Input, Text, useSelect } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import { PasswordInput } from "../components/ui/password-input";
 import { Field } from "../components/ui/field";
 import { Button } from "../components/ui/button";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { registerUser } from "../features/authActions.js";
 
 export default function Register() {
-  const [password, setPassword] = useState("");
-  const [formInput, setFormInput] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-    successMsg: "",
-  });
+  const { loading, userInfo, error, success } = useSelector(
+    (state) => state.auth
+  );
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // if (success) navigate("/login");
+    // if (userInfo) navigate("/chat");
+  }, [navigate, userInfo, success]);
+
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm<registerData>();
+  interface registerData {
+    nickname: string;
+    password: string;
+    email: string;
+  }
+  const submitForm: SubmitHandler<registerData> = (data) => {
+    console.log("registered:", data);
+    data.email = data.email.toLowerCase();
+    dispatch(registerUser(data));
+  };
   return (
     <Flex justifyContent={"center"} alignContent={"center"} marginY="auto">
       <Box
         width="80"
         height="55vh"
         margin="auto"
-        display="flex"
         alignItems={"center"}
-        background={"#F22C5D"}
+        display="flex"
+        justifyContent={"center"}
+        background={"white"}
+        color="black"
         rounded="md"
       >
         <Box padding="5">
-          <Heading size="2xl">Sign up </Heading>
-          <Text>
+          <Heading fontSize="3xl" marginBottom="-2">
+            Sign up{" "}
+          </Heading>
+          <Text marginY="6" fontSize={"lg"}>
             Log In <Link to="/login">here</Link>
           </Text>
-          <form>
-            <Field label="username" required>
+          <form method="post" onSubmit={handleSubmit(submitForm)}>
+            <Field label="nickname" required>
               <Input
                 placeholder="John Doe"
                 size="md"
-                name="username"
+                {...register("nickname")}
                 css={{ "--focus-color": "#db2777" }}
                 marginBottom="5"
               />
@@ -43,14 +66,14 @@ export default function Register() {
               <Input
                 placeholder="example@domain.com"
                 size="md"
-                name="email"
+                {...register("email")}
                 css={{ "--focus-color": "#db2777" }}
                 marginBottom="5"
               />
             </Field>
             <Field label="Password" required>
               <PasswordInput
-                onChange={(e) => setPassword(e.target.value)}
+                {...register("password")}
                 name="password"
                 id="password"
               />
