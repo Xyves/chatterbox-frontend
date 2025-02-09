ea5cc209213859e99b72590bb0b8ce8c3c428e78
Xyves <xodrixx@protonmail.com>
2025-02-09 21:45:16 +0100

    Create Chat input

--- src/components/Chat/ChatWindow/ChatInput.tsx
+++ src/components/Chat/ChatWindow/ChatInput.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import { Box, Button, Input } from "@chakra-ui/react";
+import React, { useEffect, useRef, useState } from "react";
+import EmojiPicker from "emoji-picker-react";
 
 export default function ChatInput() {
-  return <div className="mt-auto flex justify-end items-end">ChatInput</div>;
+  const [pickerOpen, setPickerOpen] = useState(false);
+  const pickerRef = useRef(null);
+  const togglePicker = () => setPickerOpen((prev) => !prev);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (pickerRef.current && !pickerRef.current.contains(event.target)) {
+        setPickerOpen(false); // Close the picker if clicked outside
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+  return (
+    <Box position="relative" display="inline-block " width="3/4">
+      <Input placeholder="Say something..." variant="subtle" />
+    </Box>
+  );
 }
