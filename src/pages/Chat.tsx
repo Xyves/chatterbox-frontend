c4e9adeabec1251cb30080488e399b3694d5a7bb
Xyves <xodrixx@protonmail.com>
2025-02-14 21:55:49 +0100

    Refactor chat page

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -10,12 +10,16 @@ import {
 import "primeicons/primeicons.css";
 
 import { Card } from "@chakra-ui/react";
-import Friendlist from "../components/Chat/Friendlist";
+import Friendlist from "../components/Chat/Sidebar/Friendlist";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUser } from "../features/authActions";
 import { Avatar } from "../components/ui/avatar";
 import Loading from "../components/Loading";
+import User from "../components/Chat/ChatWindow/User";
+import MessageList from "../components/Chat/ChatWindow/MessageList";
+import UserInfo from "../components/Chat/Sidebar/UserInfo";
+import ChatInput from "../components/Chat/ChatWindow/ChatInput";
 
 export default function Chat() {
   const dispatch = useDispatch();
@@ -34,149 +38,39 @@ export default function Chat() {
   return (
     <Grid templateColumns={"12"} height={"88vh"} width="full">
       <Box
+        overflowY="auto"
         height="auto"
         gridColumnStart="1"
         gridColumnEnd="2"
         background={"yellow.400"}
       >
-        <Card.Root>
-          <Card.Header />
-          <Card.Body>
-            {/* <Avatar
-              marginX="auto"
-              size="2xl"
-              src={
-                user.avatar_url
-                  ? user.avatar_url
-                  : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png"
-              }
-            ></Avatar> */}
-          </Card.Body>
-          <Card.Title
-            textAlign={"center"}
-            fontWeight={"bolder"}
-            fontSize="lg"
-            marginBottom="6"
-          >
-            {user.nickname}
-            {/* nickname */}
-          </Card.Title>
-        </Card.Root>
+        <UserInfo user={user} />
         <Friendlist />
       </Box>
       <Box
-        gridColumnStart={"3"}
-        background={"red.200"}
-        height="full"
+        gridColumnStart="3"
+        background="red.200"
         gridColumnEnd="12"
+        rounded="2xl"
+        height="11/12"
       >
-        <Box margin="16" background="green.600" height="2xl">
-          <Grid>
-            <Box
-              bg="blue.200"
-              height="40"
-              borderTop="0"
-              borderBottom={"6"}
-              border={"yellow"}
-              borderBlock={"solid"}
-            >
-              {/* Friend info */}
-              <Box padding="6" width="full">
-                <Flex alignItems={"center"}>
-                  <Box>
-                    <Avatar
-                      boxSize={"28"}
-                      src={
-                        user
-                          ? user.avatar_url
-                          : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png"
-                      }
-                    />
-                    <Heading
-                      display="inline-block"
-                      marginLeft={"5"}
-                      marginTop="2"
-                    >
-                      {user.nickname.charAt(0).toUpperCase() +
-                        String(user.nickname).slice(1)}
-                    </Heading>
-                  </Box>
-                  <Box
-                    marginLeft="auto"
-                    marginRight={"12"}
-                    display="flex"
-                    justifyContent={"space-between"}
-                  >
-                    <i
-                      className="pi pi-search w-full h-full "
-                      style={{ fontSize: "5rem", color: "orange" }}
-                    ></i>
-                    <i
-                      className="pi pi-phone w-full h-full "
-                      style={{ fontSize: "5rem", color: "green" }}
-                    ></i>
-                  </Box>
-                </Flex>
-              </Box>
-            </Box>
-            <Box>
-              {/* Chat log list*/}
-              <Box
-                bg="pink"
-                width="full"
-                display="flex"
-                gap="5"
-                paddingX="8"
-                marginBottom={"4"}
-              >
-                <Avatar
-                  boxSize={"20"}
-                  // size="2xl"
-                  display="flex"
-                  src={
-                    user
-                      ? user.avatar_url
-                      : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png"
-                  }
-                />
-                <Box display="inline">
-                  <Heading size="lg">
-                    {user.nickname} January 10, 2023 16:23
-                  </Heading>
-                  <Text>no way!</Text>
-                </Box>
-              </Box>
-              <Box bg="pink" width="full" display="flex" gap="5">
-                <Avatar
-                  boxSize={"14"}
-                  // size="2xl"
-                  display="flex"
-                  src={
-                    user
-                      ? user.avatar_url
-                      : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png"
-                  }
-                />
-                <Box display="inline">
-                  <Heading size="lg">
-                    {user.nickname} January 10, 2023 16:23
-                  </Heading>
-                  <Text>no way!</Text>
-                </Box>
-              </Box>
-            </Box>
-            <Box>{/* Chat input */}</Box>
-          </Grid>
-        </Box>
-        <Box>
-          <Flex direction={"row"}>
-            {/* <AvatarIcon></AvatarIcon> */}
-            <Text>{user.bio}</Text>
-            <Text>{user.avatar_url}</Text>
-          </Flex>
+        <Box
+          margin="16"
+          background="green.600"
+          height="2xl"
+          display="flex"
+          flexDirection="column"
+        >
+          <User user={user} />
+          <Box flex="1" overflow="auto">
+            <MessageList user={user} />
+            <MessageList user={user} />
+          </Box>
+          <Box width="full " padding="6">
+            <ChatInput />
+          </Box>
         </Box>
       </Box>
-      {/* </Box> */}
     </Grid>
   );
 }
