e6abd48c17501b2af2e90b47a9412b793d77370c
Xyves <xodrixx@protonmail.com>
2025-02-19 21:56:23 +0100

    Feat: add route for individual chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { defaultSystem } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router";
@@ -10,8 +11,19 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ErrorPage from "./pages/ErrorPage";
 import { ProtectedRoute } from "./pages/ProtectedRoute";
-
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { logout } from "./features/authSlice";
 function App() {
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   const token = localStorage.getItem("userToken"); // Check token in local storage
+
+  //   if (!token) {
+  //     dispatch(logout()); // Clear state if no token exists
+  //   }
+  // }, [dispatch]);
   return (
     <ChakraProvider value={defaultSystem}>
       <BrowserRouter>
@@ -23,17 +35,18 @@ function App() {
             <Route
               path="/chat"
               element={
-                <ProtectedRoute>
-                  <Chat />
-                </ProtectedRoute>
+                // <ProtectedRoute>
+                <Chat />
+                // </ProtectedRoute>
               }
             ></Route>
+            <Route path="/chat/:id" element={<Chat />}></Route>
             <Route
               path="/profile/:id"
               element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
+                // <ProtectedRoute>
+                <Profile />
+                // </ProtectedRoute>
               }
             ></Route>
             <Route path="*" element={<ErrorPage />} />
