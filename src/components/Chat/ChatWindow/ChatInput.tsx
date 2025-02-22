6804958e12bed756c04c6358924ed4f3d1cb672f
Xyves <xodrixx@protonmail.com>
2025-02-22 22:31:02 +0100

    Feat: add form handling with useForm and Redux

--- src/components/Chat/ChatWindow/ChatInput.tsx
+++ src/components/Chat/ChatWindow/ChatInput.tsx
@@ -1,20 +1,50 @@
 import { Box, Button, Input } from "@chakra-ui/react";
 import { useColorMode } from "../../ui/color-mode";
-
-export default function ChatInput({ setMessages }) {
+import { SubmitHandler, useForm } from "react-hook-form";
+import { postComment } from "../../../features/chatActions";
+import { Form } from "react-hook-form";
+import { addMessage } from "../../../features/chatSlice";
+import { useDispatch } from "react-redux";
+export default function ChatInput({ setMessages, messages = [] }) {
   const { toggleColorMode, colorMode } = useColorMode();
+  const dispatch = useDispatch();
+  const { handleSubmit, register } = useForm();
 
+  const color = colorMode === "light" ? "blackAlpha.900" : "whiteAlpha.950";
+  interface submitData {
+    chat_id: string;
+    id: string;
+    content: string;
+  }
+
+  const submitForm: SubmitHandler<submitData> = async (data) => {
+    const newComment = await postComment(data);
+    console.log(messages);
+    console.log(newComment);
+    dispatch(addMessage(newComment)); // Dispatch action to Redux store
+    console.log("Updated messages:", messages);
+  };
   return (
     <>
-      <Input
-        placeholder="Say something..."
-        variant="subtle"
-        background={colorMode === "light" ? "whiteAlpha.950" : "blackAlpha.900"}
-        color={colorMode === "light" ? "blackAlpha.900" : "whiteAlpha.950"}
-      />
-      <Button bg="blue.500" rounded="xl" marginTop="1ćC">
-        Submit Message
-      </Button>
+      <form onSubmit={handleSubmit(submitForm)}>
+        <Input
+          placeholder="Say something..."
+          variant="subtle"
+          background={
+            colorMode === "light" ? "whiteAlpha.950" : "blackAlpha.900"
+          }
+          color={color}
+        />
+        <Button
+          bg="blue.500"
+          rounded="2xl"
+          marginTop="0.5"
+          color={color}
+          type="submit"
+        >
+          Submit Message
+        </Button>
+      </form>
     </>
   );
 }
--- src/components/Chat/ChatWindow/MainChat.tsx
+++ src/components/Chat/ChatWindow/MainChat.tsx
@@ -3,15 +3,18 @@ import React, { useEffect, useState } from "react";
 import User from "./User";
 import MessageList from "./MessageList";
 import ChatInput from "./ChatInput";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { fetchMessages } from "../../../features/authActions";
 
-export default function MainChat({ messages, setMessages }) {
+export default function MainChat({ messages, setMessages, selectedFriend }) {
   const { id } = useParams();
-
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  console.log("selected friend:", selectedFriend);
   const { loading, user, error } = useSelector((state) => state.auth);
+  useEffect(() => {
+    dispatch(fetchMessages());
+  }, [dispatch]);
 
   return (
     <Box
@@ -21,12 +24,12 @@ export default function MainChat({ messages, setMessages }) {
       display="flex"
       flexDirection="column"
     >
-      <User user={user} />
+      <User user={selectedFriend} />
       <Box flex="1" overflow="auto">
         <MessageList messages={messages} />
       </Box>
       <Box width="full " padding="6">
-        <ChatInput setMessages={setMessages} />
+        <ChatInput setMessages={setMessages} messages={messages} />
       </Box>
     </Box>
   );
