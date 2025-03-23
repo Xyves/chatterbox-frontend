d852091c9386df2700f1e0a31b3400912a20b086
Xyves <xodrixx@protonmail.com>
2025-03-23 21:18:43 +0100

    Style: Move github icon into a Link element

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -6,6 +6,7 @@ export default function Footer() {
   const { colorMode } = useColorMode();
 
   const bg = colorMode === "light" ? "#55c2da" : "#2C4251";
+  const color = colorMode === "light" ? "#435058" : "white";
   return (
     <Stack
       bg={bg}
@@ -16,21 +17,22 @@ export default function Footer() {
       fontSize="md"
     >
       <Stack direction="row" align="center" justifyContent={"center"}>
-        <Text color={colorMode === "light" ? "gray.900" : "gray.50"}>
-          Github:
-        </Text>
-        <i
-          className={`pi pi-github text-center `}
-          style={{
-            color: colorMode === "light" ? "#435058" : "white",
-          }}
-        ></i>
         <Link
           href="https://github.com/Xyves "
           className="text-center text-red-200 text transition-none text-inherit border-none"
           target="_blank"
         >
-          <Text color="blackAlpha.900">Xyves</Text>
+          <Text color={colorMode === "light" ? "gray.900" : "gray.50"}>
+            Github:
+          </Text>
+          <i
+            className={`pi pi-github text-center `}
+            style={{
+              color: color,
+            }}
+          ></i>
+
+          <Text color={color}>Xyves</Text>
         </Link>
       </Stack>
     </Stack>
