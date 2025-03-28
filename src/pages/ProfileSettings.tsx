f4f88a6627644ddeca349ed714283bb9546fc211
Xyves <xodrixx@protonmail.com>
2025-03-28 22:06:56 +0100

    Feat: Add page for modifying user's data

--- src/pages/ProfileSettings.tsx
+++ src/pages/ProfileSettings.tsx
@@ -1,70 +1,129 @@
-import { Box, Input } from "@chakra-ui/react";
+import { Flex, Input, Stack } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { Field } from "../components/ui/field";
 import { useState } from "react";
-const backendUrl = "http://127.0.0.1:4000/api";
-import { UserData } from "../types";
+import { AuthState, UserData } from "../types";
+import { Button } from "../components/ui/button";
+import { getUserData } from "../app/hooks/fetchUser";
+
+import { useAppSelector } from "../app/hooks";
+import { updateUser } from "../app/hooks/updateUser.js";
+
 export default function ProfileSettings() {
+  const { user } = useAppSelector((state: { auth: AuthState }) => state.auth);
+
   const [userData, setUserData] = useState<UserData>({
-    nickname: "",
     id: "",
     bio: "",
     email: "",
     avatar_url: "",
   });
-  const getUserData = async () => {
-    const token = localStorage.getItem("userToken");
+  const handleSubmit = async () => {
+    if (!user) {
+      console.error("User not found!");
+      return;
+    }
+    if (!user.id) {
+      console.error("User ID is required!");
+      return;
+    }
     try {
-      const response = await fetch(`${backendUrl}/user/me`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Failed to post comment");
-      }
+      updateUser(userData);
+      // console.log("User updated:", updatedUser);
 
-      const data = await response.json();
-      console.log("Backend response:", data);
-      setUserData(data);
-      return data;
-    } catch (error: any) {
-      if (error.response && error.response.data.message) {
-        return error.response.data.message;
-      } else {
-        return error.message;
-      }
+      setUserData({
+        id: "",
+        email: "",
+        bio: "",
+        avatar_url: "",
+      });
+    } catch (error) {
+      console.error("Failed to update user:", error);
     }
   };
+
   useEffect(() => {
-    getUserData();
+    const fetchUserData = async () => {
+      try {
+        const data = await getUserData();
+        setUserData(data as UserData);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
   }, []);
   return (
-    <Box
-      padding="
-    5"
-      bg={"green"}
-      width="1/2"
-      marginX="auto"
+    <Flex
+      justifyContent={"center"}
+      minHeight="30vh"
+      width={["80%", "60%", "55%", "45%", "25%"]}
+      bg={"darkcyan"}
+      padding={"12"}
+      position="absolute"
+      top="50%"
+      left="50%"
+      transform="translate(-50%, -50%)"
+      rounded="2xl"
     >
-      <form className="p-5">
-        <Field label="password">
-          <Input placeholder="Enter your new password"></Input>
-        </Field>
-        <Field label="email">
-          <Input placeholder={userData.email}></Input>
-        </Field>
-        <Field label="bio">
-          <Input
-            placeholder={userData.bio == "" ? "Bio is empty" : userData.bio}
-          ></Input>
-        </Field>
-        <Field label="avatar_url">
-          <Input value={userData.avatar_url}></Input>
-        </Field>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Stack gap="5" padding="5">
+          <Field
+            label={
+              userData.nickname === "Guest"
+                ? "email (Doesn't work on Guest)"
+                : "email"
+            }
+          >
+            <Input
+              placeholder={userData.email}
+              onChange={(e) =>
+                setUserData({ ...userData, email: e.target.value })
+              }
+            ></Input>
+          </Field>
+
+          <Field
+            label={
+              userData.nickname === "Guest"
+                ? "bio (Doesn't work on Guest)"
+                : "bio"
+            }
+          >
+            <Input
+              placeholder={userData.bio == "" ? "Bio is empty" : userData.bio}
+              onChange={(e) =>
+                setUserData({ ...userData, bio: e.target.value })
+              }
+            ></Input>
+          </Field>
+          <Field
+            label={
+              userData.nickname === "Guest"
+                ? "avatar_url (Doesn't work on Guest)"
+                : "avatar_url"
+            }
+          >
+            <Input
+              placeholder={userData.avatar_url}
+              onChange={(e) =>
+                setUserData({ ...userData, avatar_url: e.target.value })
+              }
+            ></Input>
+          </Field>
+        </Stack>
+        <Button width="1/2" type="submit">
+          Update
+        </Button>
       </form>
-    </Box>
+    </Flex>
   );
 }
